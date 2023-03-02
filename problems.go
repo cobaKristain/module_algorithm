6cb9b54c532007d6b7d1fe1d8b6fbc0511c8af41
Kristain Putra <kputrapar@gmail.com>
2023-03-02 20:36:34 +0700

    menambahkan fungsi BilanganKeren

--- problems.go
+++ problems.go
@@ -1,5 +1,20 @@
 package modulesalgorithm
 
-func BilangkanKeren(n int) {
+import "fmt"
 
+func BilanganKeren() string {
+	var n int
+	var count int
+	fmt.Println("Silahkan Masukkan angka yang ingin anda masukkan")
+	fmt.Scanln(&n)
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			count++
+		}
+	}
+	if count > 4 || count < 0 {
+		return "anda memasukkan bilangan Tidak Keren"
+	} else {
+		return "anda memasukkan bilangan yang keren"
+	}
 }
